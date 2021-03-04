dc58813061629d8e60947160391a163c2ae2c140
Ashleigh Udoh <ash@tickitto.com>
2021-03-04 20:48:15 +0000

    added vault.sh

---
+++ vault.sh
@@ -0,0 +1,19 @@
+genkey() {
+  out="$1"
+  entropy="${2:-1024}"
+  openssl rand $entropy > $out
+}
+
+encrypt() {
+  vault="$1"
+  key="$2"
+  openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in $vault -pass file:$key
+}
+
+decrypt() {
+  vault="$1"
+  key="$2"
+  openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in $vault -pass file:$key
+}
+
+"$@"
