d9c3d1008ba58d5add2b97d32a9b0bcfae33b537
Ashleigh Udoh <ash@tickitto.com>
2021-03-04 22:54:29 +0000

    don't autocomplete beyond the first word

--- autil-complete.sh
+++ autil-complete.sh
@@ -5,9 +5,11 @@ _autil() {
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
-  dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  script_dir="$dir/utils"
-  COMPREPLY=($(ls $script_dir | grep "^$cur"))
+  if [ "$prev" == "autil" ]; then
+    dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    script_dir="$dir/utils"
+    COMPREPLY=($(ls $script_dir | grep "^$cur"))
+  fi
 }
 
 complete -F _autil autil
