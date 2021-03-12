2365d3e4bc18b6ee46d7553e77fe3e672ec758b3
Ashleigh Udoh <ash@audoh.co.uk>
2021-03-12 00:17:31 +0000

    removed unintended globals from completer

--- completer.sh
+++ completer.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 _autil() {
-  local cur prev opts
   COMPREPLY=()
+  local cur prev dir
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
   dir="$( cd "$( dirname "$BASH_SOURCE" )" >/dev/null 2>&1 && pwd )"
   if [ "$prev" == "autil" ]; then
+    local script_dir
     script_dir="$dir/utils"
     COMPREPLY=($(ls $script_dir | grep "^$cur"))
   else
     # defer to completer script for name
+    local completer
     completer="${COMP_WORDS[1]}"
     COMPREPLY=($( \
       "$dir/completers/$completer" \
