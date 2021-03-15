970fcd9adec245d44eb8bd20078b50815cff20af
Ashleigh Udoh <ash@tickitto.com>
2021-03-15 10:26:51 +0000

    autil alias fix

--- index.sh
+++ index.sh
@@ -4,7 +4,7 @@ _autil_index() {
   local _dir
   _dir="$(dirname "$BASH_SOURCE")"
   if [ -z `which autil` ]; then
-    alias autil='bash "$_dir/autil.sh"'
+    alias autil="bash \"$_dir/autil.sh\""
   fi
   source "$_dir/completer.sh"
 
