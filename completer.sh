8ece2686494009d05db8ca1c9330458c2c00e5ea
Ashleigh Udoh <ash@audoh.co.uk>
2021-03-21 03:29:45 +0000

    completer no longer declares locals like it was visual basic

--- completer.sh
+++ completer.sh
@@ -2,18 +2,15 @@
 
 _autil() {
   COMPREPLY=()
-  local cur prev dir
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
-  dir="$( cd "$( dirname "$BASH_SOURCE" )" >/dev/null 2>&1 && pwd )"
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local prev="${COMP_WORDS[COMP_CWORD-1]}"
+  local dir="$( cd "$( dirname "$BASH_SOURCE" )" >/dev/null 2>&1 && pwd )"
   if [ "$prev" == "autil" ]; then
-    local script_dir
-    script_dir="$dir/utils"
+    local script_dir="$dir/utils"
     COMPREPLY=($(ls $script_dir | grep "^$cur"))
   else
     # defer to completer script for name
-    local completer
-    completer="${COMP_WORDS[1]}"
+    local completer="${COMP_WORDS[1]}"
     COMPREPLY=($( \
       "$dir/completers/$completer" \
       $COMP_CWORD ${COMP_WORDS[@]} \
