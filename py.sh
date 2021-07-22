c0d786950325ba3406aa3a158d12d15aad3f79e9
Ashleigh Udoh <ashleigh.udoh@sgdigital.com>
2021-07-22 21:40:25 +0100

    updated py.sh

--- py.sh
+++ py.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+	eval "$(pyenv init -)"
+fi
 alias py="python"
