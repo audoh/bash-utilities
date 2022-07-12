5d99f7db30413ec3060b35632897b3bc0abc7736
Ashleigh Udoh <ash@tickitto.com>
2022-07-12 11:53:25 +0100

    fix autil not appearing

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@
 _autil_index() {
   local _dir
   _dir="$(dirname "$BASH_SOURCE")"
-  if alias autil 2> /dev/null; then
+  if ! alias autil 2> /dev/null; then
     alias autil="source \"$_dir/autil.sh\""
   fi
   source "$_dir/completer.sh"
